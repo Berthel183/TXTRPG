c84161eeaf43f88a5a5c3fddf17648267f6f1eb8
Marco <132661671+Berthel183@users.noreply.github.com>
2024-04-23 17:50:25 +0200

    4 Modules
    Separato i moduli in modo da leggerli comodamente, soprattutto per il modulo 'functions'

--- TXTRPG.py
+++ TXTRPG.py
@@ -1,28 +1,7 @@
-import random
-from scelta_utente import get_user_choice
-
-class Enemy:
-    def __init__(self,name,hp,minatk,maxatk,exp):
-        self.name = name
-        self.hp = hp
-        self.minatk = minatk
-        self.maxatk = maxatk
-        self.exp = exp
-
-class Player:
-    def __init__(self,name,hp,atk,exp,lv,maxhp):
-        self.name = name
-        self.hp = hp
-        self.atk = atk  
-        self.exp = exp
-        self.lv = lv      
-        self.maxhp = maxhp
+from functions import giocatore,header,start_menu,select_challenge,load_save,player_stats,settings,heal
 
 
-giocatore = Player(0,50,12,0,1,50)
-enemy = Enemy(0,0,0,0,0)
-exp_pool = 10
-lvcount = 0
+
 
 #ctrl k c / ctrl k u
 
@@ -41,146 +20,16 @@ print("""
       attaccare (tasto 1) e spera di vincere!
       """)
 
-
-def header():
-        print("Ti stai approcciando al magico mondo di TXTRPG, ma ho bisogno di sapere il tuo nome!")
-        nome_iniziale = str(input(">"))
-        print("Ti vuoi chiamare,",nome_iniziale,"? 1 per confermare, 2 per inserire un altro nome")
-        choice = get_user_choice("",2)
-
-        if choice == 2:
-            print("Hai confermato il tuo nome!")
-            header("allora, ricominciamo!")
-        elif choice == 1:        
-            print("Hai confermato il tuo nome!")
-    
-        giocatore.name = nome_iniziale
-        return giocatore
 header()
-
-
-def player_stats(exp_pool):
-    
-    if giocatore.exp < exp_pool:
-        print("Ciao",giocatore.name,", ecco le tue statistiche!")
-        print("Vita massima:",giocatore.maxhp)
-        print("Vita:",giocatore.hp)
-        print("Attacco:",giocatore.atk)
-        print("livello:",giocatore.lv)
-        print("Esperienza:",giocatore.exp,"\n")
-        from menu import start_menu
-        
-    #for player_exp in exp_pool:
-    else:
-        print(exp_pool,"EXP POOL PRINT PRIMA")
-        exp_pool += 10
-        print(exp_pool,"EXP POOL PRINT")
-        giocatore.maxhp += 10
-        giocatore.atk += 4
-        print(giocatore.exp,"GIOCATORE EXP PRINT PRIMA")
-        giocatore.exp = 0
-        print(giocatore.exp,"GIOCATORE EXP PRINT")
-        giocatore.lv += lvcount
-        giocatore.hp = giocatore.maxhp
-        print("Vita:",giocatore.hp)
-        print("Attacco:",giocatore.atk)
-        print("Esperienza",giocatore.exp,"\n")
-        from menu import start_menu
-        
 
 print("E allora ti porgo il mio benvenuto""",giocatore.name,""",per ora, attaccherai solo, non ci sono altre opzioni, un mostro verrà scelto in base alla difficoltà e durante il combattimento,
     l'ordine d'attacco sarà casuale: quindi sostanzialmente continua ad attaccare e spera di vincere!""")
 
-from menu import start_menu
-start_menu()
+start_menu(select_challenge,load_save,player_stats,settings,heal)
 
-def settings():
 
-    print("""Cosa vuoi fare?
-          1) Cambia nome giocatore
-          2) esci
-          """)    
 
-    choice = choice = get_user_choice()
-    if choice == 1:
 
-        print("Come vuoi essere chiamato? Scegli un nome, non giudichiamo")
-        nome_giocatore = str(input(""))
-        print("BRAVISSIMO!! ORA TI CHIAMI ||*",nome_giocatore,"*|| !!\n")
-        input("Premi un tasto qualsiasi per continuare")
-        giocatore.name = nome_giocatore
-        settings()
-        return
-
-    elif choice == 2:
-        print("v-Prego di qua, mi segua-v")
-        from menu import start_menu
-
-    else:
-        print("immissione non valida, me stai a perculà? dai fa il serio va a sceglie")
-        settings() 
-
-def load_save():
-
-    print("Da qui potrai caricare un file con i tuoi progressi! come si chiama il file? sii molto preciso, con tanto di estensione!")
-    print("compra il dlc per salvare i progressi, per ora torna al menù")
-    input("Premi qualsiasi tasto per proseguire")
-    from menu import start_menu
-    importsave = input(">")
-
-def heal():
-    print("Un cerusico ti trova e ti riattoppa le ferite!")
-    giocatore.hp = giocatore.maxhp    
-    print("Hai",giocatore.hp,"punti ferita ora!")
-    input("press any")
-    from menu import start_menu
-
-def select_challenge():
-    
-    print("Allora",giocatore.name,"che mostro vuoi affrontare? Scegli una difficoltà tra 1, 2 o 3, 4 se popo t'aregge, invio per confermare o CTRL+C per uscire")
-    enemylist1 = ("Topo","Pesce","Sedia","Tizio")
-    enemylist2 = ("Scheletro","Mannaro","Gorillone","Mago")
-    enemylist3 = ("Drago","Lich","Golem","Flayer")
-    enemylist4 = ("GOD")
-    
-    challenge = int(input(">"))
-    
-    if challenge == 1:
-        enemy.name = random.choice(enemylist1)
-        enemy.hp = random.randint(50,80)
-        enemy.minatk = 5
-        enemy.maxatk = 10
-        enemy.exp = 5
-        #drop_item_weapon
-        #drop_item_armor
-
-    elif challenge == 2:
-        enemy.name = random.choice(enemylist2)
-        enemy.hp = random.randint(80,120)
-        enemy.minatk = 15
-        enemy.maxatk = 20
-        enemy.exp = 10
-       
-    elif challenge == 3:
-        enemy.name = random.choice(enemylist3)
-        enemy.hp = random.randint(130,170)
-        enemy.minatk = 20
-        enemy.maxatk = 30
-        enemy.exp = 20
-
-    elif challenge == 4:
-        enemy.name = (enemylist4)
-        enemy.hp = random.randint(9999,9999)
-        enemy.minatk = 666
-        enemy.maxatk = 1337
-        enemy.exp = 500
-        
-    else:
-        print("immissione non valida, te risparo il pippone, premi qualcosa")
-        input(">cojone<")
-        select_challenge()
-
-    return enemy
  
 
 
---
+++ functions.py
@@ -0,0 +1,215 @@
+#Funzioni contenute:header, player_stats, settings, load_save, heal, select_challenge, 
+from scelta_utente import get_user_choice
+import random
+
+class Enemy:
+    def __init__(self,name,hp,minatk,maxatk,exp):
+        self.name = name
+        self.hp = hp
+        self.minatk = minatk
+        self.maxatk = maxatk
+        self.exp = exp
+
+class Player:
+    def __init__(self,name,hp,atk,exp,lv,maxhp):
+        self.name = name
+        self.hp = hp
+        self.atk = atk  
+        self.exp = exp
+        self.lv = lv      
+        self.maxhp = maxhp
+
+
+giocatore = Player(0,50,12,0,1,50)
+enemy = Enemy(0,0,0,0,0)
+exp_pool = 10
+lvcount = 0
+
+#Intro e primo input per creazione nome
+def header():
+    print("Ti stai approcciando al magico mondo di TXTRPG, ma ho bisogno di sapere il tuo nome!")
+    nome_iniziale = str(input(">"))
+    print("Ti vuoi chiamare,",nome_iniziale,"? 1 per confermare, 2 per inserire un altro nome")
+    choice = get_user_choice("",2)
+
+    if choice == 2:
+        print("Hai confermato il tuo nome!")
+        header("allora, ricominciamo!")
+    elif choice == 1:        
+        print("Hai confermato il tuo nome!")
+    
+    giocatore.name = nome_iniziale
+    return giocatore
+
+#Menù principale di gioco
+def start_menu(select_challenge, load_save, player_stats, settings, heal):
+    
+    print("""
+    Quindi,""",giocatore.name,""",Ora che siamo pronti, scegli cosa fare!
+    Premi:
+
+    1) Gioca!
+    2) Carica salvataggio(work in progress)
+    3) Statistiche giocatore
+    4) Impostazioni
+    5) Esci
+    6) Heal
+    """)
+    choice = int(input(">"))
+    
+    if choice == 1:
+        if giocatore.hp <= 0:
+            print("DOVE CAZZO VAI CO",giocatore.hp,"DI VITA?? CURATI",input("/>Premi un tasto!"))
+            start_menu()
+        else:
+            select_challenge()
+
+    elif choice == 2: 
+        load_save()
+
+    elif choice == 3:
+        player_stats(exp_pool)
+
+    elif choice == 4:
+        nome = settings()
+        giocatore.name = nome
+        
+    elif choice == 5:
+        print("Sei sicuro di voler uscire? ti caghi? 1 Per uscire 0 per tornare al menu")
+        close = int(input(">"))
+        if close == 1:
+            print("a merda")
+            input("")
+            exit()
+        elif close == 0:
+            print("v-Prego di qua, mi segua-v")
+            start_menu()
+    
+    elif choice == 6:
+        heal()
+
+    else:
+        print("immissione non valida, daje mpò")
+        start_menu()   
+
+#STATISTICHE PLAYER
+def player_stats(exp_pool):
+    
+    if giocatore.exp < exp_pool:
+        print("Ciao",giocatore.name,", ecco le tue statistiche!")
+        print("Vita massima:",giocatore.maxhp)
+        print("Vita:",giocatore.hp)
+        print("Attacco:",giocatore.atk)
+        print("livello:",giocatore.lv)
+        print("Esperienza:",giocatore.exp,"\n")
+        from menu import start_menu
+        
+    #for player_exp in exp_pool:
+    else:
+        print(exp_pool,"EXP POOL PRINT PRIMA")
+        exp_pool += 10
+        print(exp_pool,"EXP POOL PRINT")
+        giocatore.maxhp += 10
+        giocatore.atk += 4
+        print(giocatore.exp,"GIOCATORE EXP PRINT PRIMA")
+        giocatore.exp = 0
+        print(giocatore.exp,"GIOCATORE EXP PRINT")
+        giocatore.lv += lvcount
+        giocatore.hp = giocatore.maxhp
+        print("Vita:",giocatore.hp)
+        print("Attacco:",giocatore.atk)
+        print("Esperienza",giocatore.exp,"\n")
+        
+#IMPOSTAZIONI
+def settings():
+
+    print("""Cosa vuoi fare?
+          1) Cambia nome giocatore
+          2) esci
+          """)    
+
+    choice = choice = get_user_choice()
+    if choice == 1:
+
+        print("Come vuoi essere chiamato? Scegli un nome, non giudichiamo")
+        nome_giocatore = str(input(""))
+        print("BRAVISSIMO!! ORA TI CHIAMI ||*",nome_giocatore,"*|| !!\n")
+        input("Premi un tasto qualsiasi per continuare")
+        giocatore.name = nome_giocatore
+        settings()
+        return
+
+    elif choice == 2:
+        print("v-Prego di qua, mi segua-v")
+        from menu import start_menu
+
+    else:
+        print("immissione non valida, me stai a perculà? dai fa il serio va a sceglie")
+        settings() 
+
+#CARICA SALVATAGGIO - WORK IN PROGRESS -
+def load_save():
+
+    print("Da qui potrai caricare un file con i tuoi progressi! come si chiama il file? sii molto preciso, con tanto di estensione!")
+    print("compra il dlc per salvare i progressi, per ora torna al menù")
+    input("Premi qualsiasi tasto per proseguire")
+    from menu import start_menu
+    importsave = input(">")
+
+#CURA, SET HP TO MAX
+def heal():
+    print("Un cerusico ti trova e ti riattoppa le ferite!")
+    giocatore.hp = giocatore.maxhp    
+    print("Hai",giocatore.hp,"punti ferita ora!")
+    input("press any")
+    from menu import start_menu
+
+#SELEZIONE DIFFICOLTA' E SCELTA NEMICO
+def select_challenge():
+    print("Allora",giocatore.name,"che mostro vuoi affrontare? Scegli una difficoltà tra 1, 2 o 3, 4 se popo t'aregge, invio per confermare o CTRL+C per uscire")
+    enemylist1 = ("Topo","Pesce","Sedia","Tizio")
+    enemylist2 = ("Scheletro","Mannaro","Gorillone","Mago")
+    enemylist3 = ("Drago","Lich","Golem","Flayer")
+    enemylist4 = ("GOD")
+    
+    challenge = int(input(">"))
+    
+    if challenge == 1:
+        enemy.name = random.choice(enemylist1)
+        enemy.hp = random.randint(50,80)
+        enemy.minatk = 5
+        enemy.maxatk = 10
+        enemy.exp = 5
+        #drop_item_weapon
+        #drop_item_armor
+
+    elif challenge == 2:
+        enemy.name = random.choice(enemylist2)
+        enemy.hp = random.randint(80,120)
+        enemy.minatk = 15
+        enemy.maxatk = 20
+        enemy.exp = 10
+       
+    elif challenge == 3:
+        enemy.name = random.choice(enemylist3)
+        enemy.hp = random.randint(130,170)
+        enemy.minatk = 20
+        enemy.maxatk = 30
+        enemy.exp = 20
+
+    elif challenge == 4:
+        enemy.name = (enemylist4)
+        enemy.hp = random.randint(9999,9999)
+        enemy.minatk = 666
+        enemy.maxatk = 1337
+        enemy.exp = 500
+        
+    else:
+        print("immissione non valida, te risparo il pippone, premi qualcosa")
+        input(">cojone<")
+        select_challenge()
+
+    return enemy
+
+
+
